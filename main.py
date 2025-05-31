798642af20d4e345107f4c3dd31dc03915d49712
owais08 <muhammadowais0811@gmail.com>
2025-06-01 03:15:26 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,43 @@
+from agents import Agent, Runner, AsyncOpenAI, OpenAIChatCompletionsModel, RunConfig
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+gemini_api_key = os.getenv("GEMINI_API_KEY")
+
+# Check if the API key is present; if not, raise an error
+if not gemini_api_key:
+    raise ValueError("GEMINI_API_KEY is not set. Please ensure it is defined in your .env file.")
+
+#Reference: https://ai.google.dev/gemini-api/docs/openai
+external_client = AsyncOpenAI(
+    api_key=gemini_api_key,
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/",
+)
+
+model = OpenAIChatCompletionsModel(
+    model="gemini-2.0-flash",
+    openai_client=external_client
+)
+
+config = RunConfig(
+    model=model,
+    model_provider=external_client,
+    tracing_disabled=True
+)
+
+# Writer Agent
+writer_agent = Agent(
+    name = 'Writer Agent',
+    instructions= 
+    """You are a writer agent. Generate poem,
+    stories, essay, email etc."""
+)
+
+response = Runner.run_sync(
+    writer_agent,
+    input = input("Enter a message: "),
+    run_config = config
+    )
+print(response.final_output)
